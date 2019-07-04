1904a1c3be5a0dc6b9e60c7e1cb5ed90b8a14012
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-04 21:07:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,9 +49,19 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  true
 end
 
 def reduce_to_any_true(source_array)
-
+  i = 0
+  while i < source_array.length do
+    return true if !source_array[i]
+    i += 1
+  end
+  false
 end
